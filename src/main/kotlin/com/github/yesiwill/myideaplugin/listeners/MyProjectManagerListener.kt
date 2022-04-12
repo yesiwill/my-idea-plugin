1604bd41e0ff3396f97c2bd91596c18ce63d6cca
GitHub Action <action@github.com>
2022-04-12 12:14:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/yesiwill/myideaplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/yesiwill/myideaplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yesiwill.myideaplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yesiwill.myideaplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/yesiwill/myideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yesiwill/myideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yesiwill.myideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yesiwill.myideaplugin.MyBundle
 
 class MyApplicationService {
 
